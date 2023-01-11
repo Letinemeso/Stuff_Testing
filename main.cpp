ca7860ad1f63c5c65e969beb55978f2f7c8734a7
a.hiryanov <a.hiryanov@octaviangameart.ru>
2023-01-11 14:57:37 +0300

    ASS

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 
+#include <thread>
+
+#include <Data_Structures/Map.h>
+#include <Stuff/Stopwatch.h>
+
 int main()
 {
+	LDS::Map<std::string, int> map_test;
 
+//	for(unsigned int i=0; i<10000; ++i)
+
+//	map_test.insert("ass", 1);
+//	map_test.insert("bass", 3);
+//	map_test.insert("balaclava", 16);
+//	map_test.insert("poui", 999);
+
+	LST::Stopwatch sw;
+
+	sw.start();
+
+	std::this_thread::sleep_for(std::chrono::seconds(2));
+
+	sw.stop();
+
+	std::cout << sw.duration() << "\n";
 
 	return 0;
 }
