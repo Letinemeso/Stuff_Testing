9de89a68091c1f15ead725a4395e6a6fb67718c5
a.hiryanov <a.hiryanov@octaviangameart.ru>
2023-01-13 18:05:19 +0300

    testing, duh

--- main.cpp
+++ main.cpp
@@ -1,47 +1,38 @@
 #include <iostream>
 #include <iomanip>
+#include <map>
 
 #include <thread>
 
 #include <Data_Structures/Map.h>
 #include <Stuff/Stopwatch.h>
 
+
 int main()
 {
-	LDS::Map<int, std::string> lds_map_test;
 
-//	for(unsigned int i=0; i<10000; ++i)
-
-//	lds_map_test.insert("ass", 1);
-//	lds_map_test.insert("bass", 3);
-//	lds_map_test.insert("balaclava", 16);
-//	lds_map_test.insert("poui", 999);
+	LDS::Map<std::string, int> lds_map_test;
 
 	std::setprecision(3);
-
 
 	LST::Stopwatch sw;
 
 	sw.start();
 
-	for(unsigned int i=0; i<10000; ++i)
-		lds_map_test.insert(i, std::to_string(i));
+	for(int i=0; i<100000; ++i)
+		lds_map_test.insert(std::to_string(i), i);
 
 	sw.stop();
 
 	std::cout << std::fixed << "LDS::Map::insert:\t" << sw.duration() << "\n";
 
-	std::map<int, std::string> std_map_test;
-
 	sw.start();
 
-	for(unsigned int i=0; i<10000; ++i)
-		std_map_test.emplace(i, std::to_string(i));
+	LDS::Map<std::string, int>::Iterator ldsit = lds_map_test.find("1000000");
 
 	sw.stop();
 
-	std::cout << std::fixed << "std::map::emplace:\t" << sw.duration() << "\n";
-
+	std::cout << std::fixed << "LDS::Map::find:\t" << sw.duration() << "\n";
 
 	sw.start();
 
@@ -49,7 +40,26 @@ int main()
 
 	sw.stop();
 
-	std::cout << std::fixed << "LDS::Map::clear:\t" << sw.duration() << "\n";
+	std::cout << std::fixed << "LDS::Map::clear:\t" << sw.duration() << "\n\n";
+
+	std::map<std::string, int> std_map_test;
+
+	sw.start();
+
+	for(int i=0; i<100000; ++i)
+		std_map_test.emplace(std::to_string(i), i);
+
+	sw.stop();
+
+	std::cout << std::fixed << "std::map::emplace:\t" << sw.duration() << "\n";
+
+	sw.start();
+
+	std::map<std::string, int>::iterator stdit = std_map_test.find("1000000");
+
+	sw.stop();
+
+	std::cout << std::fixed << "std::map::find:\t" << sw.duration() << "\n";
 
 
 	sw.start();
@@ -60,6 +70,15 @@ int main()
 
 	std::cout << std::fixed << "std::map::clear:\t" << sw.duration() << "\n";
 
+
+//	while(true)
+//	{
+//		LDS::Map<int, std::string> map;
+//		for(int i=0; i<100000; ++i)
+//			map.insert(i, std::to_string(i));
+//		map.clear();
+//		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+//	}
 
 
 	return 0;
