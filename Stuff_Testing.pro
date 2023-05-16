c6efc6d2a34eb9d52f62c4056ac197b8cc80a060
a.hiryanov <a.hiryanov@octaviangameart.ru>
2023-05-16 18:16:25 +0300

    testing LGL::Pathfinder, and it seems to work great

--- Stuff_Testing.pro
+++ Stuff_Testing.pro
@@ -7,11 +7,14 @@ SOURCES += \
         main.cpp
 
 INCLUDEPATH += "../L_Utility/include"
+INCLUDEPATH += "../L_Gamedev_Lib/include"
 
 win32 {
     LIBS += "../../L_Utility/Build/debug/libL_Utility.a"
+    LIBS += "../../L_Gamedev_Lib/Build/debug/libL_Gamedev_Lib.a"
 }
 
 unix {
     LIBS += "../../L_Utility/Build/libL_Utility.a"
+    LIBS += "../../L_Gamedev_Lib/Build/libL_Gamedev_Lib.a"
 }
--- main.cpp
+++ main.cpp
@@ -12,22 +12,144 @@
 
 #include <Stuff/Function_Wrapper.h>
 
+#include <Graph/Pathfinder.h>
+#include <vector>
+
+LGL::Graph construct_graph(std::vector<std::vector<char>> field)
+{
+    LGL::Graph result;
+    result.allocate_nodes(field.size() * field[0].size());
+
+    auto calculate_index = [field](int x, int y)->int
+    {
+        unsigned int width = field.size();
+        unsigned int height = field[0].size();
+
+        if(x < 0 || x >= width || y < 0 || y >= height)
+            return -1;
+
+        return x * height + y;
+    };
+
+    auto link_with_surrounding = [&](int x, int y)
+    {
+        int index = calculate_index(x, y);
+
+        int left = calculate_index(x - 1, y);
+        int right = calculate_index(x + 1, y);
+        int top = calculate_index(x, y + 1);
+        int bottom = calculate_index(x, y - 1);
+
+        if(left != -1)
+        {
+            if(field[x - 1][y] == 1)
+                result.link_nodes(index, left, 10, false);
+        }
+
+        if(right != -1)
+        {
+            if(field[x + 1][y] == 1)
+                result.link_nodes(index, right, 10, false);
+        }
+
+        if(top != -1)
+        {
+            if(field[x][y + 1] == 1)
+                result.link_nodes(index, top, 10, false);
+        }
+
+        if(bottom != -1)
+        {
+            if(field[x][y - 1] == 1)
+                result.link_nodes(index, bottom, 10, false);
+        }
+    };
+
+    for(int x=0; x<(int)field.size(); ++x)
+    {
+        for(int y=0; y<(int)field[0].size(); ++y)
+        {
+            link_with_surrounding(x, y);
+        }
+    }
+
+    return result;
+}
 
 int main()
 {
-    LDS::Vector<int> vec;
+    std::vector<std::vector<char>> field =
+    {
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {0, 0, 0, 0, 0, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 0, 0, 0, 0, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    };
 
-    vec.resize(200);
+    LGL::Graph graph = construct_graph(field);
 
-    for(int i=0; i<50; ++i)
-        vec.push(i);
+    LGL::Pathfinder pathfinder(&graph,
+                               [field](unsigned int _id_1, unsigned int _id_2)
+    {
+        unsigned int width = field.size();
+        unsigned int height = field[0].size();
 
-    vec[150] = 999;
+        int x1 = _id_1 / height;
+        int y1 = _id_1 & height;
 
-    for(unsigned int i=0; i<50; ++i)
-        std::cout << vec[i] << "\n";
+        int x2 = _id_2 / height;
+        int y2 = _id_2 & height;
 
-    std::cout << vec[150] << "\n";
+        return abs(x1 - x2) + abs(y1 - y2);
+    });
+
+    pathfinder.process(0, 99);
+
+    for(unsigned int i=0; i<pathfinder.path_result().size(); ++i)
+    {
+        std::cout << pathfinder.path_result()[i] << " ";
+
+        unsigned int width = field.size();
+        unsigned int height = field[0].size();
+
+        int x = pathfinder.path_result()[i] / height;
+        int y = pathfinder.path_result()[i] % height;
+
+        field[x][y] = '.';
+    }
+    std::cout << "\n\n";
+
+    unsigned int width = field.size();
+    unsigned int height = field[0].size();
+
+    for(unsigned int x=0; x<width + 2; ++x)
+        std::cout << "##";
+    std::cout << "\n";
+    for(unsigned int y=0; y < height; ++y)
+    {
+        std::cout << "# ";
+//        for(int y=height - 1; y >= 0; --y)
+        for(unsigned int x=0; x<width; ++x)
+        {
+            if(field[y][x] == 1)
+                field[y][x] = ' ';
+            if(field[y][x] == 0)
+                field[y][x] = '#';
+
+            std::cout << field[y][x] << ' ';
+        }
+        std::cout << " #\n";
+    }
+    for(unsigned int x=0; x<width + 2; ++x)
+        std::cout << "##";
+    std::cout << "\n";
+    std::cout << "\n";
 
 	return 0;
 }
